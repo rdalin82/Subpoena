19152c7ba8e040f1b6616a6c23c21fec60ecf932
rdalin82 <Robert.Dalin@gmail.com>
2015-08-16 11:39:18 -0500

    unfinished main controller

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -3,4 +3,13 @@ class MainsController < ApplicationController
 		@providers = Provider.all
 		@attorneys = Attorney.all 
 	end 
+
+	private 
+	def attorney_params
+		params.require(:attorney).permit(:name, :address, :fax, :phone)
+	end 
+
+	def provider_params
+		params.require(:provider).permit(:name, :doctor, :firstlineaddress, :secondlineaddress, :citystatezip)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'mains#index'
 
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   #get '/attorneys' => 'attorneys#index'
