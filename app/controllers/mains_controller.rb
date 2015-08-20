ae4c25813df32f008d12a98901e6c6a8986dd439
rdalin82 <Robert.Dalin@gmail.com>
2015-08-19 19:30:51 -0500

    working gsub but not all fields

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -14,16 +14,24 @@ class MainsController < ApplicationController
 		
 		@provider = Provider.find(params['providers'].to_i)
 		@attorney = Attorney.find(params['attorneys'].to_i)
-		f = File.new("#{Rails.root}/public/files/"+Time.now.to_s+@provider.name+ ".txt", "w")
-		f.write("#{@provider.name} \n" + "#{@provider.firstlineaddress} \n" + "#{@provider.citystatezip}")
-		f.write("\n")
-		f.write("#{@attorney.name}, #{@attorney.address}, phone: #{@attorney.phone}" )
-		f.close 
+		f = File.read("#{Rails.root}/public/template/subpoena.rtf")
+		s = f
+		s = s.gsub('<attyname>', @attorney.name)
+		s = s.gsub('<attyaddress>', @attorney.address)
+		s = s.gsub('<attyphone>', @attorney.phone)
+		s = s.gsub('<providername', @provider.name)
+		s = s.gsub('<provideraddress>', @provider.firstlineaddress)
+		s = s.gsub('<providercitystatezip>', @provider.citystatezip)
+		File.write("#{Rails.root}/public/files/"+Time.now.to_s+@provider.name+ ".rtf", s)
+		#f.write("#{@provider.name} \n" + "#{@provider.firstlineaddress} \n" + "#{@provider.citystatezip}")
+		#f.write("\n")
+		#f.write("#{@attorney.name}, #{@attorney.address}, phone: #{@attorney.phone}" )
+		
 		redirect_to root_path
 	end 
 
 	def download
-		send_file "#{Rails.root}/public/files/"+params[:id], :type => 'text/plain'
+		send_file "#{Rails.root}/public/files/"+params[:id], :type => 'application/msword'
 	end
 
 	private 
