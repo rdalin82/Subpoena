627738857818c330e649b6945ad009c052b0f827
rdalin82 <Robert.Dalin@gmail.com>
2015-08-23 08:04:26 -0500

    added model tests, included minitest/autorun to use let blocks and describe it specs

--- test/models/attorney_test.rb
+++ test/models/attorney_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class AttorneyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	describe Attorney do 
+		let(:attorney) {Attorney.new} 
+		let(:attorney1) { Attorney.create!(name: "Laura Hartin", address: "100 W. Randolph Chicago, IL 60601", phone: "312-814-1442")}
+	   it "should not save blank attorney" do 
+	     refute attorney.save
+	   end  
+		it "should save with valid name, address, phone" do 
+			assert attorney1.save 
+		end
+	end 
 end
--- test/models/provider_test.rb
+++ test/models/provider_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class ProviderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	describe Provider do 
+		let(:provider) {Provider.new} 
+		let(:provider1) {Provider.create(name: "Some Janky Hospital", firstlineaddress: "100 Take all your money Lane", citystatezip: "South Chicago, IL 60606")}
+
+		it "should not save a blank provider" do 
+			refute provider.save 
+		end 
+
+		it "should save a valid provider" do 
+			assert provider1.save 
+		end 
+	end 
 end
