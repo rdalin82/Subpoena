3fdca412366a6f3b20804e97886c05d9c8c1b05c
rdalin82 <Robert.Dalin@gmail.com>
2015-08-23 09:50:50 -0500

    added basic menu

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'mains#index'
+  get '/' => 'mains#index', as: 'mains'
   get '/new' => 'mains#new'
   post '/create' => 'mains#create'
   get '/download' => 'mains#download'
