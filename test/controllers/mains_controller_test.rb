41d368f36d98b5e63bab4fb1adbea606f5221092
rdalin82 <Robert.Dalin@gmail.com>
2015-11-07 15:03:12 -0600

    huh?

--- test/controllers/mains_controller_test.rb
+++ test/controllers/mains_controller_test.rb
@@ -3,7 +3,10 @@ require 'test_helper'
 class MainsControllerTest < ActionController::TestCase
 	def setup 
 		sign_in users(:one)
+
 	end 
+
+	
 	test "should get index" do 
 		get :index 
 		assert_response :success 
@@ -15,7 +18,15 @@ class MainsControllerTest < ActionController::TestCase
 	end 
 
 	test 'should redirect on create' do
-		post :create
-		assert_response :success {'providers'=> 1, 'attorneys'=>1 }
+		post :create, {"providers"=>"1",
+ "attorneys"=>"1",
+ "n1"=>"",
+ "n2"=>"",
+ "employee"=>"",
+ "employer"=>"",
+ "docket"=>"",
+ "returndate"=>"",
+ "todaysdate"=>""}
+		assert_response :success 
 	end 
 end
