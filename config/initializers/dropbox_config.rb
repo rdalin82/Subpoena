9dd5ff756ecaa49f06dd4766ce47f78bab0a4a2b
rdalin82 <Robert.Dalin@gmail.com>
2016-05-14 12:55:37 -0500

    dropbox

--- config/initializers/dropbox_config.rb
+++
@@ -1,4 +0,0 @@
-require "dropbox_sdk"
-DROPBOX_APP_KEY = "uf1rqukssytvnd4" #replace with your own app key
-DROPBOX_APP_KEY_SECRET = "5hjnoquh1xvdu05" #replace with your own key secret
-DROPBOX_APP_MODE = "dropbox"
